6293ac85eaedc9b0402b7da31e46e584948f96e7
carina1754 <bulebed1754@yonsei.ac.kr>
2022-11-10 03:10:13 +0900

    UperNet using FocalNet and noise del

---
+++ models/UperNet.py
@@ -0,0 +1,141 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+from itertools import chain
+import logging
+import torch.nn as nn
+import numpy as np
+from torchinfo import summary
+from FocalNet import *
+import os
+import torch
+import torch.nn as nn
+import numpy as np
+import math
+import PIL
+
+def initialize_weights(*models):
+    for model in models:
+        for m in model.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight.data, nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1.)
+                m.bias.data.fill_(1e-4)
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0.0, 0.0001)
+                m.bias.data.zero_()
+
+class BaseModel(nn.Module):
+    def __init__(self):
+        super(BaseModel, self).__init__()
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+    def forward(self):
+        raise NotImplementedError
+
+    def summary(self):
+        model_parameters = filter(lambda p: p.requires_grad, self.parameters())
+        nbr_params = sum([np.prod(p.size()) for p in model_parameters])
+        self.logger.info(f'Nbr of trainable parameters: {nbr_params}')
+
+    def __str__(self):
+        model_parameters = filter(lambda p: p.requires_grad, self.parameters())
+        nbr_params = sum([np.prod(p.size()) for p in model_parameters])
+        return super(BaseModel, self).__str__() + f'\nNbr of trainable parameters: {nbr_params}'
+        #return summary(self, input_shape=(2, 3, 224, 224))
+
+
+class PSPModule(nn.Module):
+    # In the original inmplementation they use precise RoI pooling 
+    # Instead of using adaptative average pooling
+    def __init__(self, in_channels, bin_sizes=[1, 2, 4, 6]):
+        super(PSPModule, self).__init__()
+        out_channels = in_channels // len(bin_sizes)
+        self.stages = nn.ModuleList([self._make_stages(in_channels, out_channels, b_s) 
+                                                        for b_s in bin_sizes])
+        self.bottleneck = nn.Sequential(
+            nn.Conv2d(in_channels+(out_channels * len(bin_sizes)), in_channels, 
+                                    kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(0.1)
+        )
+
+    def _make_stages(self, in_channels, out_channels, bin_sz):
+        prior = nn.AdaptiveAvgPool2d(output_size=bin_sz)
+        conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
+        bn = nn.BatchNorm2d(out_channels)
+        relu = nn.ReLU(inplace=True)
+        return nn.Sequential(prior, conv, bn, relu)
+    
+    def forward(self, features):
+        h, w = features.size()[2], features.size()[3]
+        pyramids = [features]
+        pyramids.extend([F.interpolate(stage(features), size=(h, w), mode='bilinear', 
+                                        align_corners=True) for stage in self.stages])
+        output = self.bottleneck(torch.cat(pyramids, dim=1))
+        return output
+
+def up_and_add(x, y):
+    return F.interpolate(x, size=(y.size(2), y.size(3)), mode='bilinear', align_corners=True) + y
+
+class FPN_fuse(nn.Module):
+    def __init__(self, feature_channels=[256, 512, 1024, 2048], fpn_out=256):
+        super(FPN_fuse, self).__init__()
+        assert feature_channels[0] == fpn_out
+        self.conv1x1 = nn.ModuleList([nn.Conv2d(ft_size, fpn_out, kernel_size=1)
+                                    for ft_size in feature_channels[1:]])
+        self.smooth_conv =  nn.ModuleList([nn.Conv2d(fpn_out, fpn_out, kernel_size=3, padding=1)] 
+                                    * (len(feature_channels)-1))
+        self.conv_fusion = nn.Sequential(
+            nn.Conv2d(len(feature_channels)*fpn_out, fpn_out, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(fpn_out),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, features):
+        
+        features[1:] = [conv1x1(feature) for feature, conv1x1 in zip(features[1:], self.conv1x1)]
+        P = [up_and_add(features[i], features[i-1]) for i in reversed(range(1, len(features)))]
+        P = [smooth_conv(x) for smooth_conv, x in zip(self.smooth_conv, P)]
+        P = list(reversed(P))
+        P.append(features[-1]) #P = [P1, P2, P3, P4]
+        H, W = P[0].size(2), P[0].size(3)
+        P[1:] = [F.interpolate(feature, size=(H, W), mode='bilinear', align_corners=True) for feature in P[1:]]
+
+        x = self.conv_fusion(torch.cat((P), dim=1))
+        return x
+
+class UperNet_L(BaseModel):
+    # Implementing only the object path
+    def __init__(self, num_classes, in_channels=3, fpn_out=64, **_):
+        super(UperNet_L, self).__init__()
+
+        feature_channels = [64, 128, 256, 512]
+        self.backbone = FocalNet()
+        self.PPN = PSPModule(feature_channels[-1])
+        self.FPN = FPN_fuse(feature_channels, fpn_out=fpn_out)
+        self.head = nn.Conv2d(fpn_out, num_classes, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        input_size = (x.size()[2], x.size()[3])
+
+        features = self.backbone(x)
+        print(features)
+        features[-1] = self.PPN(features[-1])
+        x = self.head(self.FPN(features))
+        x = F.interpolate(x, size=input_size, mode='bilinear')
+        return x
+
+    def get_backbone_params(self):
+        return self.backbone.parameters()
+
+    def get_decoder_params(self):
+        return chain(self.PPN.parameters(), self.FPN.parameters(), self.head.parameters())
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = UperNet_L(num_classes=4).to(device)
+    summary(model,(16,3, 256, 480))
---
+++ noise_del.py
@@ -0,0 +1,16 @@
+import os
+
+noise_img = ['2017_KAG_2LB_000912.png', '2019_KBG_3LB_000073.png', '2019_KDG_2LB_000871.png', '2019_WSN_2LB_000047.png', '2019_JNG_1LB_000005.png', '2019_KSG_2LB_000374.png', '2019_SMG_1LB_000027.png']
+
+noise_2n3 = ['2015_DMG_2LB_000436.png', '2016_YDP_JJG_000151.png', '2017_KSG_SAG_000148.png', '2017_YDP_2LB_000503.png', '2018_KAG_SAG_000041.png', '2018_KSG_JJG_000007.png', '2018_SMG_3LB_000185.png', '2018_SPG_3LB_000326.png', '2019_JNG_SAG_000234.png', '2019_JRG_SAG_000081.png', '2019_KSG_2LB_000544.png', '2019_KSG_SAG_000123.png', '2019_MPG_3LB_000229.png']
+
+noise_1n2 = ['2016_YDP_JJG_000075.png', '2018_SCG_2LB_000288.png', '2018_SCG_2LB_000292.png', '2019_JNG_1LB_000129.png', '2019_KSG_2LB_000589.png', '2019_KSG_KNI_000461.png']
+data_list = [noise_img,noise_2n3,noise_1n2]
+file_list = ['x','y']
+for noise in range(len(data_list)):
+    for i in data_list[noise]:
+        for k in file_list:
+            file_path='./data/train/'+k+'/'+i
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+                print(file_path)
